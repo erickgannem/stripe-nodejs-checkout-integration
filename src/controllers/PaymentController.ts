dea824f85d154708a52dc0c8be172fc0b935971b
Erick Gannem <erickgannem@gmail.com>
2020-09-24 12:52:00 -0400

    [PaymentController]: add feature to create payment intent

--- src/controllers/PaymentController.ts
+++ src/controllers/PaymentController.ts
@@ -1,5 +1,6 @@
 import { Stripe } from 'stripe';
 import { Request, Response } from 'express';
+import { errorMonitor } from 'stream';
 
 const { STRIPE_API_SECRET_KEY }: {STRIPE_API_SECRET_KEY: string} = process.env;
 
@@ -7,6 +8,16 @@ const stripe = new Stripe(STRIPE_API_SECRET_KEY, { apiVersion: '2020-08-27' });
 
 export default class PaymentController {
   static async create(req: Request, res: Response) {
-    return res.json(process.env.STRIPE_API_SECRET_KEY);
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        // amount placeholder
+        amount: 1000,
+        currency: 'brl',
+        metadata: { integration_check: 'accept_a_payment' },
+      });
+      res.status(200).json({ client_secret: paymentIntent.client_secret });
+    } catch (error) {
+      res.status(400).json({ error });
+    }
   }
 }
