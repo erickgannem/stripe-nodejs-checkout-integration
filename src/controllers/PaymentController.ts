163bbf87ddd957c7903c7e8acd6b1f0f5e40e8a6
Erick Gannem <erickgannem@gmail.com>
2020-09-25 23:00:54 -0400

    endpoint fixes

--- src/controllers/PaymentController.ts
+++ src/controllers/PaymentController.ts
@@ -7,10 +7,10 @@ const stripe = new Stripe(STRIPE_API_SECRET_KEY, { apiVersion: '2020-08-27' });
 
 export default class PaymentController {
   static async create(req: Request, res: Response) {
+    const { amount }:{amount: number} = req.body;
     try {
       const paymentIntent = await stripe.paymentIntents.create({
-        // amount placeholder
-        amount: 1000,
+        amount,
         currency: 'brl',
         metadata: { integration_check: 'accept_a_payment' },
       });
