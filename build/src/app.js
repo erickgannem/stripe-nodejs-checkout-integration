bf543f99757b7495d551af835d210353338e7b9d
Erick Gannem <erickgannem@gmail.com>
2020-11-01 17:10:56 -0400

    prepare for deploy

---
+++ build/src/app.js
@@ -0,0 +1,29 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var dotenv_1 = __importDefault(require("dotenv"));
+var morgan_1 = __importDefault(require("morgan"));
+var routes_1 = __importDefault(require("./routes"));
+dotenv_1.default.config({});
+var App = /** @class */ (function () {
+    function App() {
+        this.server = express_1.default();
+        this.middlewares();
+        this.routes();
+    }
+    App.prototype.routes = function () {
+        this.server.use(routes_1.default);
+    };
+    App.prototype.middlewares = function () {
+        this.server.use(morgan_1.default('tiny'));
+        this.server.use(cors_1.default());
+        this.server.use(express_1.default.json());
+    };
+    return App;
+}());
+var app = new App();
+exports.default = app;
