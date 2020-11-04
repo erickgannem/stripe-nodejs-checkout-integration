0df367cc52caa5d79f188700769f1585c1949197
Erick Gannem <erickgannem@gmail.com>
2020-11-04 12:00:38 -0400

    prod testing

--- src/controllers/PaymentController.ts
+++ src/controllers/PaymentController.ts
@@ -2,13 +2,15 @@ import { Stripe } from 'stripe';
 import { Request, Response } from 'express';
 import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
-const { STRIPE_API_SECRET_KEY } = process.env;
-const stripe = new Stripe(STRIPE_API_SECRET_KEY, { apiVersion: '2020-08-27' });
+const { STRIPE_API_SECRET_KEY_TEST } = process.env;
+
+const stripe = new Stripe(STRIPE_API_SECRET_KEY_TEST, { apiVersion: '2020-08-27' });
 
 export default class PaymentController {
   static async create(req: Request, res: Response) {
+    console.log(STRIPE_API_SECRET_KEY_TEST);
     try {
       const { amount, paymentData } = req.body;
       if (!paymentData) {
